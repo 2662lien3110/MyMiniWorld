82972eda1a1a8e23243940c36d3c0363f2aeff62
2662lien3110 <61580621+2662lien3110@users.noreply.github.com>
2020-05-23 16:07:05 +0200

    Add files via upload

--- A2C1.py
+++ A2C1.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.DEBUG)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class Agent(object):
     def __init__(self, **kwargs):
-        self.lr_act = 9e-4
+        self.lr_act = 3e-5
         self.lr_crit = 0
         self.batch_size = 64
         self.atoms = 80
