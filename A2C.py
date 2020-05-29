4d6c7c3935c8a636bf21eb11abeffb34d85408c5
2662lien3110 <61580621+2662lien3110@users.noreply.github.com>
2020-05-29 21:39:13 +0200

    Update A2C.py

--- A2C.py
+++ A2C.py
@@ -26,7 +26,7 @@ class Agent(object):
         self.actions = 3
         self.channels = 9
         self.gamma = 0.65
-        self.lambdaEntrop = 0.05
+        self.lambdaEntrop = 0.0
         self.lambdaCrit = 0.41667
         self.weightDecay = False
         self.actor = CNNBase(self.channels, self.actions, self.atoms)
