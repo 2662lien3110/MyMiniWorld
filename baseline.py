98aea9dadcd7376f12af3ab0e690168c7eb90dca
2662lien3110 <61580621+2662lien3110@users.noreply.github.com>
2020-06-07 17:17:40 +0200

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -44,7 +44,7 @@ class Agent(object):
         self.lr = 3e-4
         self.batch_size = 64
         self.gamma = 0.999
-        self.epsilon = 0.1
+        self.epsilon = 0.8
         self.Vmin = -25
         self.Vmax = 25
         self.atoms = 80
@@ -78,13 +78,14 @@ class Agent(object):
         #print("update target")
 
     def updata_epsilon(self, rew):
-        self.reward.append(rew)
+        if rew>0:
+            self.reward.append(rew)
         #print("update epsilon")
         if len(self.reward) > 25:#100:
             self.epsilon = 0.1
         elif len(self.reward) > 15: #60:
             self.epsilon = 0.2
-        elif np.sum(self.reward) > 0:
+        elif np.sum(self.reward) > 5:
             self.epsilon = max(0.4, self.epsilon * 0.8)
 
     def projection_distribution(self, next_state, reward, done, gam):
