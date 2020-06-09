a56ebdd776a817d35ed1de99ab681cb7225ed800
2662lien3110 <61580621+2662lien3110@users.noreply.github.com>
2020-06-09 18:17:53 +0200

    Update A2C2.py

--- A2C2.py
+++ A2C2.py
@@ -27,7 +27,7 @@ class Agent(object):
         self.actions = 3
         self.channels = 9
         self.gamma = 0.65
-        self.lambdaEntrop = 0.4
+        self.lambdaEntrop = 0.99
         print(self.lambdaEntrop)
         self.lambdaCrit = 0.41667
         self.weightDecay = False
