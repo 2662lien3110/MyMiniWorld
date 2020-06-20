8c8309c8520ea14d88b5f4068ca64e32905bbdb6
2662lien3110 <61580621+2662lien3110@users.noreply.github.com>
2020-06-20 16:36:06 +0200

    Update A2C.py

--- A2C.py
+++ A2C.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.DEBUG)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class Agent(object):
     def __init__(self, **kwargs):
-        self.lr_act = 0.003
+        self.lr_act = 0.035
         print(self.lr_act)
         self.lr_crit = 0
         self.batch_size = 64
@@ -27,7 +27,7 @@ class Agent(object):
         self.actions = 3
         self.channels = 9
         self.gamma = 0.65
-        self.lambdaEntrop = 0.25
+        self.lambdaEntrop = 0.35
         print(self.lambdaEntrop)
         self.lambdaCrit = 0.41667
         self.weightDecay = False
